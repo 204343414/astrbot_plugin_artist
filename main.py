be4cea93a3f2bad5b032cb2faf42ee76ce49b993
204343414 <107094185+204343414@users.noreply.github.com>
2026-07-19 15:51:52 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1471,8 +1471,8 @@ class GeminiArtist(Star):
     @filter.llm_tool(name="generate_self_reaction")
     async def generate_self_reaction(self, event: AstrMessageEvent, scene_description: str) -> AsyncGenerator[Any, None]:
         '''
-        对方希望画你自己时优先调用此画图工具而不是使用draw，用于魔改或表情包等。诸如与用户进行互动整活时使用。
-        该函数会自动使用你角色的参考图，比文字提示词精确，你只需描述场景动作等，绝对不要额外描述你的外貌特征！！！！
+        对方希望画你自己时优先调用此画图工具。在明确了此要求的情况下绝对不要使用draw工具！用于魔改或表情包等。诸如与用户进行互动整活时使用。
+        该函数会自动使用你角色的参考图，比文字提示词精确。
 
         Args:
             scene_description (string): 画面描述。包括你的表情、情绪、动作、与对方当前的互动等。
